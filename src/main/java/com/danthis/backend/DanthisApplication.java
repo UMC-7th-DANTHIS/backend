b24fca5b77c0595dcef6464bad6342d01cb94e5a
junseoplee <junseoplee@soongsil.ac.kr>
2024-12-31 05:18:44 +0900

    Initial commit

---
+++ src/main/java/com/danthis/backend/DanthisApplication.java
@@ -0,0 +1,13 @@
+package com.danthis.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanthisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DanthisApplication.class, args);
+  }
+
+}
