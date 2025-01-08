d4b7035375f1bc8d58a84c53161167bcdd77b5c0
Minhyuk Song <poiuy010724@naver.com>
2025-01-08 16:35:58 +0900

    feat: Dancer와 DancerGenre, DancerIamge 간 양방향 매핑 추가 (#2)

--- src/main/java/com/danthis/backend/domain/dancer/Dancer.java
+++ src/main/java/com/danthis/backend/domain/dancer/Dancer.java
@@ -1,12 +1,16 @@
 package com.danthis.backend.domain.dancer;
 
 import com.danthis.backend.domain.BaseEntity;
+import com.danthis.backend.domain.dancerimage.DancerImage;
+import com.danthis.backend.domain.mapping.dancergenre.DancerGenre;
 import com.danthis.backend.domain.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
 
 @Entity
 @Table(name = "dancers")
@@ -42,4 +46,10 @@ public class Dancer extends BaseEntity {
     private String openChatUrl;
 
     private Boolean isApproved;
+
+    @OneToMany(mappedBy = "dancer", fetch = FetchType.LAZY)
+    private Set<DancerGenre> dancerGenres;
+
+    @OneToMany(mappedBy = "dancer", fetch = FetchType.LAZY)
+    private Set<DancerImage> dancerImages;
 }
