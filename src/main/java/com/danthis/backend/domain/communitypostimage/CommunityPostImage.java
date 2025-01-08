cb3d2af1849a0ce8be60fc25a192c95822016fec
Minhyuk Song <poiuy010724@naver.com>
2025-01-08 17:48:42 +0900

    feat: CommunityPostImage 엔티티 생성 (#5)

---
+++ src/main/java/com/danthis/backend/domain/communitypostimage/CommunityPostImage.java
@@ -0,0 +1,28 @@
+package com.danthis.backend.domain.communitypostimage;
+
+import com.danthis.backend.domain.BaseEntity;
+import com.danthis.backend.domain.communitypost.CommunityPost;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "communityPostImages")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class CommunityPostImage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId")
+    private CommunityPost post;
+}
