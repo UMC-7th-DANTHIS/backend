0419f04f0f711c5e61adc63bdacd77b4fd880a37
Jangwon0319 <ryujw0319@gmail.com>
2025-01-08 17:00:35 +0900

    feat: ClassReview 엔티티 생성 (#4)

---
+++ src/main/java/com/danthis/backend/domain/classreview/repository/ClassReviewRepositoryImpl.java
@@ -0,0 +1,10 @@
+package com.danthis.backend.domain.classreview.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ClassReviewRepositoryImpl {
+
+}
