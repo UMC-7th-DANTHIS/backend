174e78305e140581848f9ef643962f22faa23f7c
Minhyuk Song <poiuy010724@naver.com>
2025-01-08 17:48:17 +0900

    feat: CommunityPost 엔티티 생성 (#5)

---
+++ src/main/java/com/danthis/backend/domain/communitypost/CommunityPost.java
@@ -0,0 +1,42 @@
+package com.danthis.backend.domain.communitypost;
+
+import com.danthis.backend.domain.BaseEntity;
+import com.danthis.backend.domain.communitycomment.CommunityComment;
+import com.danthis.backend.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "communityPosts")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class CommunityPost extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ColumnDefault("0")
+    private Integer views;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @OneToMany(mappedBy = "communityPost", fetch = FetchType.LAZY)
+    private Set<CommunityComment> communityComments;
+}
