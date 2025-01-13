51c87c3ba7d136b65f641fb7301155b477dd2f69
Jangwon0319 <ryujw0319@gmail.com>
2025-01-13 22:46:37 +0900

    feat: Genre 관련 양방향 매핑 관계 설정 (#15)

--- src/main/java/com/danthis/backend/domain/genre/Genre.java
+++ src/main/java/com/danthis/backend/domain/genre/Genre.java
@@ -1,7 +1,17 @@
 package com.danthis.backend.domain.genre;
 
 import com.danthis.backend.domain.BaseEntity;
-import jakarta.persistence.*;
+import com.danthis.backend.domain.danceclass.DanceClass;
+import com.danthis.backend.domain.mapping.dancergenre.DancerGenre;
+import com.danthis.backend.domain.mapping.usergenre.UserGenre;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,10 +24,19 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Genre extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(nullable = false, length = 20)
-    private String name;
+  @Column(nullable = false, length = 20)
+  private String name;
+
+  @OneToMany(mappedBy = "genre", fetch = FetchType.LAZY)
+  private Set<DanceClass> danceClasses;
+
+  @OneToMany(mappedBy = "genre", fetch = FetchType.LAZY)
+  private Set<DancerGenre> dancerGenres;
+
+  @OneToMany(mappedBy = "genre", fetch = FetchType.LAZY)
+  private Set<UserGenre> userGenres;
 }
