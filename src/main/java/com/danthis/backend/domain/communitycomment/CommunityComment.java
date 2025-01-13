ac7469f0d6ade008417ffe1cfa0d73e781005e4e
Jangwon0319 <ryujw0319@gmail.com>
2025-01-13 22:52:06 +0900

    feat: Community 관련 양방향 매핑 관계 설정 (#15)

--- src/main/java/com/danthis/backend/domain/communitycomment/CommunityComment.java
+++ src/main/java/com/danthis/backend/domain/communitycomment/CommunityComment.java
@@ -3,7 +3,14 @@ package com.danthis.backend.domain.communitycomment;
 import com.danthis.backend.domain.BaseEntity;
 import com.danthis.backend.domain.communitypost.CommunityPost;
 import com.danthis.backend.domain.user.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,18 +23,18 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CommunityComment extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(nullable = false)
-    private String content;
+  @Column(nullable = false)
+  private String content;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "post_id", nullable = false)
-    private CommunityPost post;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id", nullable = false)
+  private CommunityPost post;
 }
--- src/main/java/com/danthis/backend/domain/communitypost/CommunityPost.java
+++ src/main/java/com/danthis/backend/domain/communitypost/CommunityPost.java
@@ -2,15 +2,23 @@ package com.danthis.backend.domain.communitypost;
 
 import com.danthis.backend.domain.BaseEntity;
 import com.danthis.backend.domain.communitycomment.CommunityComment;
+import com.danthis.backend.domain.communitypost.communitypostimage.CommunityPostImage;
 import com.danthis.backend.domain.user.User;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
-
-import java.util.Set;
 
 @Entity
 @NoArgsConstructor
@@ -19,23 +27,26 @@ import java.util.Set;
 @Builder
 public class CommunityPost extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Column(nullable = false)
-    private String title;
+  @Column(nullable = false)
+  private String title;
 
-    @Column(nullable = false)
-    private String content;
+  @Column(nullable = false)
+  private String content;
 
-    @ColumnDefault("0")
-    private Integer views;
+  @ColumnDefault("0")
+  private Integer views;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
 
-    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
-    private Set<CommunityComment> communityComments;
+  @OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
+  private Set<CommunityComment> communityComments;
+
+  @OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
+  private Set<CommunityPostImage> communityPostImages;
 }
