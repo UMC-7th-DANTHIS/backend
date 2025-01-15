98c3e468dccd6cb066c3b71bc7cc877bf8f20c46
junseoplee <junseoplee@soongsil.ac.kr>
2025-01-16 04:18:28 +0900

    refactor: 코드 스타일 컨벤션 수정 (#10)

--- src/main/java/com/danthis/backend/domain/mapping/dancergenre/DancerGenre.java
+++ src/main/java/com/danthis/backend/domain/mapping/dancergenre/DancerGenre.java
@@ -4,23 +4,27 @@ import com.danthis.backend.domain.BaseEntity;
 import com.danthis.backend.domain.dancer.Dancer;
 import com.danthis.backend.domain.genre.Genre;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 public class DancerGenre extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "dancer_id")
-    private Dancer dancer;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "dancer_id")
+  private Dancer dancer;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "genre_id")
-    private Genre genre;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "genre_id")
+  private Genre genre;
 }
