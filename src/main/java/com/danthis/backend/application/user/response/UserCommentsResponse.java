b55946c18e600deb82a53e6e2d4be80881dbdddc
MinHyuk SONG <poiuy010724@naver.com>
2025-01-22 02:48:01 +0900

    feature: reponseDto 생성 (#25)

---
+++ src/main/java/com/danthis/backend/application/user/response/UserCommentsResponse.java
@@ -0,0 +1,31 @@
+package com.danthis.backend.application.user.response;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserCommentsResponse {
+
+  private List<CommentDto> posts;
+  private PaginationDto pagination;
+
+  @Getter
+  @Builder
+  public static class CommentDto {
+
+    private Long id;
+    private Long postId;
+    private String content;
+  }
+
+  @Getter
+  @Builder
+  public static class PaginationDto {
+
+    private Integer currentPage;
+    private Integer totalPages;
+    private Long totalResults;
+  }
+}
