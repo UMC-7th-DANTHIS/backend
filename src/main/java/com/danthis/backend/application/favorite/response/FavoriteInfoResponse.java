377f1538ff9b2bb9bb5daf16c489bd3e93529810
MinHyuk SONG <poiuy010724@naver.com>
2025-01-27 10:16:52 +0900

    feature: 찜한 수업 response DTO 생성 (#30)

--- src/main/java/com/danthis/backend/application/favorite/response/FavoriteInfoResponse.java
+++ src/main/java/com/danthis/backend/application/favorite/response/FavoriteInfoResponse.java
@@ -27,6 +27,24 @@ public class FavoriteInfoResponse {
 
   @Getter
   @Builder
+  public static class WishListInfo {
+
+    List<DanceClassInfo> danceClasses;
+    Pagination pagination;
+  }
+
+  @Getter
+  @Builder
+  public static class DanceClassInfo {
+
+    private Long id;
+    private String className;
+    private String dancerName;
+    private List<String> images;
+  }
+
+  @Getter
+  @Builder
   public static class Pagination {
 
     private Integer currentPage;
