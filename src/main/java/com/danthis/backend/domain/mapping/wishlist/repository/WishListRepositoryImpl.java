322712a13a50603a9811c9b14a5a87369de76e78
MinHyuk SONG <poiuy010724@naver.com>
2025-01-27 10:17:31 +0900

    feature: 찜한 수업 조회 Repository Custom 생성 (#30)

--- src/main/java/com/danthis/backend/domain/mapping/wishlist/repository/WishListRepositoryImpl.java
+++ src/main/java/com/danthis/backend/domain/mapping/wishlist/repository/WishListRepositoryImpl.java
@@ -1,10 +1,30 @@
 package com.danthis.backend.domain.mapping.wishlist.repository;
 
+import com.danthis.backend.domain.mapping.wishlist.QWishList;
+import com.danthis.backend.domain.mapping.wishlist.WishList;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class WishListRepositoryImpl {
+public class WishListRepositoryImpl implements WishListRepositoryCustom {
 
+  private final JPAQueryFactory jpaQueryFactory;
+  private final QWishList wishList = QWishList.wishList;
+
+  @Override
+  public Page<WishList> findByUserId(Long userId, Pageable pageable) {
+    List<WishList> wishLists = jpaQueryFactory.selectFrom(wishList)
+                                              .where(wishList.id.eq(userId))
+                                              .offset(pageable.getOffset())
+                                              .limit(pageable.getPageSize())
+                                              .fetch();
+
+    return new PageImpl<>(wishLists, pageable, wishLists.size());
+  }
 }
