9176c4841e3b93af0c9e8dd5ede3ff41be9da1c8
Jangwon0319 <ryujw0319@gmail.com>
2025-02-17 23:27:25 +0900

    style: 코드 정리(#48)

--- src/main/java/com/danthis/backend/domain/mapping/danceclassbooking/repository/DanceClassBookingRepositoryCustom.java
+++ src/main/java/com/danthis/backend/domain/mapping/danceclassbooking/repository/DanceClassBookingRepositoryCustom.java
@@ -1,13 +1,8 @@
 package com.danthis.backend.domain.mapping.danceclassbooking.repository;
 
 import com.danthis.backend.domain.danceclass.DanceClass;
-import com.danthis.backend.domain.mapping.danceclassbooking.DanceClassBooking;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface DanceClassBookingRepositoryCustom {
-
-  Page<DanceClassBooking> findApprovedBookingsByClass(DanceClass danceClass, Pageable pageable);
 
   void deleteByDanceClass(DanceClass danceClass);
 }
--- src/main/java/com/danthis/backend/domain/mapping/danceclassbooking/repository/DanceClassBookingRepositoryImpl.java
+++ src/main/java/com/danthis/backend/domain/mapping/danceclassbooking/repository/DanceClassBookingRepositoryImpl.java
@@ -3,14 +3,9 @@ package com.danthis.backend.domain.mapping.danceclassbooking.repository;
 import static com.danthis.backend.domain.mapping.danceclassbooking.QDanceClassBooking.danceClassBooking;
 
 import com.danthis.backend.domain.danceclass.DanceClass;
-import com.danthis.backend.domain.mapping.danceclassbooking.DanceClassBooking;
 import com.danthis.backend.domain.mapping.danceclassbooking.QDanceClassBooking;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -20,38 +15,10 @@ public class DanceClassBookingRepositoryImpl implements DanceClassBookingReposit
   private final JPAQueryFactory jpaQueryFactory;
 
   @Override
-  public Page<DanceClassBooking> findApprovedBookingsByClass(DanceClass danceClass,
-      Pageable pageable) {
-    List<DanceClassBooking> results = jpaQueryFactory
-        .selectFrom(danceClassBooking)
-        .where(
-            danceClassBooking.danceClass.eq(danceClass)
-                                        .and(danceClassBooking.isActive.eq(true))
-                                        .and(danceClassBooking.isApproved.eq(true))
-        )
-        .offset(pageable.getOffset())
-        .limit(pageable.getPageSize())
-        .fetch();
-
-    long total = jpaQueryFactory
-        .select(danceClassBooking.count())
-        .from(danceClassBooking)
-        .where(
-            danceClassBooking.danceClass.eq(danceClass)
-                                        .and(danceClassBooking.isActive.eq(true))
-                                        .and(danceClassBooking.isApproved.eq(true))
-        )
-        .fetchOne();
-
-    return new PageImpl<>(results, pageable, total);
-  }
-
-  @Override
   public void deleteByDanceClass(DanceClass danceClass) {
     jpaQueryFactory.delete(QDanceClassBooking.danceClassBooking)
                    .where(QDanceClassBooking.danceClassBooking.danceClass.eq(danceClass)
-                                                                         .and(
-                                                                             danceClassBooking.isActive.eq(true)))
+                                                                         .and(danceClassBooking.isActive.eq(true)))
                    .execute();
   }
 }
