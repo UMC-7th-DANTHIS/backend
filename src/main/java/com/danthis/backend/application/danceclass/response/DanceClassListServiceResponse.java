b0572ca9e012cfc427054a20397669088aeff4c4
Jangwon0319 <ryujw0319@gmail.com>
2025-02-20 15:18:07 +0900

    fix: 수업 썸네일 고정(#87)

--- src/main/java/com/danthis/backend/application/danceclass/response/DanceClassListServiceResponse.java
+++ src/main/java/com/danthis/backend/application/danceclass/response/DanceClassListServiceResponse.java
@@ -1,6 +1,8 @@
 package com.danthis.backend.application.danceclass.response;
 
 import com.danthis.backend.domain.danceclass.DanceClass;
+import com.danthis.backend.domain.danceclass.danceclassimage.DanceClassImage;
+import java.util.Comparator;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -32,16 +34,20 @@ public class DanceClassListServiceResponse {
                                         .totalElements(danceClassPage.getTotalElements())
                                         .danceClasses(danceClassPage.getContent().stream()
                                                                     .map(danceClass -> DanceClassSummary.builder()
-                                                                                                       .id(danceClass.getId())
-                                                                                                       .className(danceClass.getClassName())
-                                                                                                       .dancerName(danceClass.getDancer().getDancerName())
-                                                                                                       .thumbnailImage(danceClass.getDanceClassImages()
-                                                                                                                     .stream()
-                                                                                                                     .findFirst()
-                                                                                                                     .map(image -> image.getImageUrl())
-                                                                                                                     .orElse(null))
-                                                                                                       .build())
+                                                                                                        .id(danceClass.getId())
+                                                                                                        .className(danceClass.getClassName())
+                                                                                                        .dancerName(danceClass.getDancer().getDancerName())
+                                                                                                        .thumbnailImage(getFixedThumbnailImage(danceClass))
+                                                                                                        .build())
                                                                     .toList())
                                         .build();
+  }
+
+  private static String getFixedThumbnailImage(DanceClass danceClass) {
+    return danceClass.getDanceClassImages().stream()
+                     .sorted(Comparator.comparing(DanceClassImage::getId))
+                     .map(DanceClassImage::getImageUrl)
+                     .findFirst()
+                     .orElse(null);
   }
 }
