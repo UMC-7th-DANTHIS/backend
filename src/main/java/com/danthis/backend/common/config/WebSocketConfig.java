83e6c7eea66013321fc02d3bf6a3b1daecfeec2e
Jangwon0319 <ryujw0319@gmail.com>
2025-05-14 22:58:22 -0700

    feat: WebSocketConfig 추가 (#95)

---
+++ src/main/java/com/danthis/backend/common/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.danthis.backend.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+  private final WebSocketHandler webSocketHandler;
+
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(webSocketHandler, "/ws/chat")
+            .setAllowedOrigins("*") // "/ws/chat" 경로로 웹소켓 연결 허용
+            .addInterceptors(new HttpSessionHandshakeInterceptor());  // HTTP 세션 정보를 웹소켓으로 전달
+    //TODO: JWT 검증 인터셉터 추가
+  }
+}
