96cd4ab24934870a2cbd16c278407ab2c94943ff
SongMinHyuk <poiuy010724@naver.com>
2025-05-21 11:04:40 +0900

    bugfix: requestMatcher options 경로 수정 (#96)

--- src/main/java/com/danthis/backend/common/config/SecurityConfig.java
+++ src/main/java/com/danthis/backend/common/config/SecurityConfig.java
@@ -25,46 +25,46 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-  private final JwtAuthenticationFailEntryPoint jwtAuthenticationFailEntryPoint;
-  private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
-  private final JwtFilter jwtFilter;
-  private final KakaoUserDetailsService kakaoUserDetailsService;
+    private final JwtAuthenticationFailEntryPoint jwtAuthenticationFailEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtFilter jwtFilter;
+    private final KakaoUserDetailsService kakaoUserDetailsService;
 
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .formLogin(AbstractHttpConfigurer::disable)
-        .httpBasic(AbstractHttpConfigurer::disable)
-        .csrf(AbstractHttpConfigurer::disable)
-        .cors(withDefaults())
-        .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .oauth2Login(oauth -> oauth
-            .userInfoEndpoint(config -> config.userService(kakaoUserDetailsService))
-        )
-        .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/",
-                    "/actuator/health",
-                    "/auth/**",
-                    "/exception/**",
-                    "/dance-classes/all",
-                    "/dancers/all")
-                .permitAll()
-                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .anyRequest()
-                .authenticated()
-        )
-        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-        .exceptionHandling(ex -> {
-          ex.authenticationEntryPoint(jwtAuthenticationFailEntryPoint);
-          ex.accessDeniedHandler(jwtAccessDeniedHandler);
-        });
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(withDefaults())
+                .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login(oauth -> oauth
+                        .userInfoEndpoint(config -> config.userService(kakaoUserDetailsService))
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/",
+                                "/actuator/health",
+                                "/auth/**",
+                                "/exception/**",
+                                "/dance-classes/all",
+                                "/dancers/all")
+                        .permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**/*").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(ex -> {
+                    ex.authenticationEntryPoint(jwtAuthenticationFailEntryPoint);
+                    ex.accessDeniedHandler(jwtAccessDeniedHandler);
+                });
 
-    return http.build();
-  }
+        return http.build();
+    }
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
