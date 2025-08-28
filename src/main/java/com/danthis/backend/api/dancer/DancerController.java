4d500c10b4ba6cbf396169a6b1817562b04275c8
S0NGMinHyuk <poiuy010724@naver.com>
2025-08-28 19:04:11 +0900

    feat: 모든 메서드에 AssignCurrentUserInfo 어노테이션 추가(#103)
    userId가 필요하지 않는 메서드는 null이 가능한 어노테이션으로 변경

--- src/main/java/com/danthis/backend/api/dancer/DancerController.java
+++ src/main/java/com/danthis/backend/api/dancer/DancerController.java
@@ -9,6 +9,7 @@ import com.danthis.backend.application.dancer.DancerService;
 import com.danthis.backend.application.dancer.response.DancerInfoResponse;
 import com.danthis.backend.application.dancer.response.DancerSummaryListResponse;
 import com.danthis.backend.common.security.aop.AssignCurrentUserInfo;
+import com.danthis.backend.common.security.aop.AssignOrNullCurrentUserInfo;
 import com.danthis.backend.common.security.aop.CurrentUserInfo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -39,6 +40,7 @@ public class DancerController {
   public ApiResponse<Long> addDancer(
       CurrentUserInfo userInfo,
       @RequestBody @Valid DancerAddRequest request) {
+
     Long dancerId = dancerService.addDancerInfo(userInfo.getUserId(), request.toServiceRequest());
     return ApiResponse.OK(dancerId);
   }
@@ -49,6 +51,7 @@ public class DancerController {
   public ApiResponse<Long> updateDancer(
       CurrentUserInfo userInfo,
       @RequestBody @Valid DancerUpdateRequest request) {
+
     Long dancerId = dancerService.updateDancerInfo(userInfo.getUserId(), request.toServiceRequest());
     return ApiResponse.OK(dancerId);
   }
@@ -58,26 +61,30 @@ public class DancerController {
   @AssignCurrentUserInfo
   public ApiResponse<DancerInfoResponse> getMyDancerInfo(
       CurrentUserInfo userInfo) {
+
     DancerInfoResponse response = dancerService.getMyDancerInfo(userInfo.getUserId());
     return ApiResponse.OK(response);
   }
 
   @Operation(summary = "단일 댄서 정보 조회 API", description = "댄서의 정보를 조회합니다.")
   @GetMapping("/{dancerId}")
-  @AssignCurrentUserInfo
+  @AssignOrNullCurrentUserInfo
   public ApiResponse<DancerInfoResponse> getDancerInfo(
       CurrentUserInfo userInfo,
       @PathVariable("dancerId") Long dancerId) {
+
     DancerInfoResponse response = dancerService.getDancerInfo(userInfo.getUserId(), dancerId);
     return ApiResponse.OK(response);
   }
 
   @Operation(summary = "장르별 댄서 정보 조회 API", description = "장르별 댄서들의 정보를 조회합니다.")
   @GetMapping("/genres/{genreId}")
+  @AssignOrNullCurrentUserInfo
   public ApiResponse<DancerSummaryListResponse> getDancersByGenre(
       @PathVariable("genreId") Long genreId,
       @RequestParam(defaultValue = "1") @Min(1) Integer page,
       @RequestParam(defaultValue = "9") @Min(1) Integer size) {
+
     DancerSummaryListResponse response = dancerService.getDancersByGenre(genreId, page, size);
     return ApiResponse.OK(response);
   }
@@ -97,7 +104,9 @@ public class DancerController {
 
   @Operation(summary = "모든 댄서 정보 조회 API", description = "모든 댄서의 정보를 조회합니다.")
   @GetMapping("/all")
+  @AssignOrNullCurrentUserInfo
   public ApiResponse<DancerSummaryListResponse> getAllDancers() {
+
     DancerSummaryListResponse response = dancerService.getAllDancers();
     return ApiResponse.OK(response);
   }
@@ -105,7 +114,9 @@ public class DancerController {
   @Operation(summary = "사용자 장르 맞춤 댄서 추천 API", description = "사용자의 선호 장르를 기반으로 해당 장르를 주장르로 하는 댄서를 조회합니다.")
   @GetMapping("/recommendations")
   @AssignCurrentUserInfo
-  public ApiResponse<DancerSummaryListResponse> getRecommendationDancers(CurrentUserInfo userInfo) {
+  public ApiResponse<DancerSummaryListResponse> getRecommendationDancers(
+      CurrentUserInfo userInfo) {
+
     DancerSummaryListResponse response = dancerService.getRecommendationDancers(userInfo.getUserId());
     return ApiResponse.OK(response);
   }
