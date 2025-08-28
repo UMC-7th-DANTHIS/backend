6cf054843c5da5be701c3a618ff4acd91af7f68a
S0NGMinHyuk <poiuy010724@naver.com>
2025-08-28 19:05:09 +0900

    feat: 사용자와 댄서 사이 isFavorite 변수값 설정 알고리즘 수정(#103)

--- src/main/java/com/danthis/backend/application/dancer/DancerService.java
+++ src/main/java/com/danthis/backend/application/dancer/DancerService.java
@@ -93,19 +93,13 @@ public class DancerService {
 
   @Transactional
   public DancerInfoResponse getMyDancerInfo(Long userId) {
-    User user = userReader.readUserById(userId);
     Dancer dancer = dancerReader.readDancerByUserId(userId);
-
-    // 이미 찜이 된 경우 true, 찜이 안되어 있으면 false 저장
-    boolean isFavorite = userDancerReader.readUserDancerByUserAndDancer(user, dancer) != null;
-
     return DancerInfoResponse.builder()
                              .id(dancer.getId())
                              .dancerName(dancer.getDancerName())
                              .instargramId(dancer.getInstargramId())
                              .bio(dancer.getBio())
                              .history(dancer.getHistory())
-                             .isFavorite(isFavorite)
                              .openChatUrl(dancer.getOpenChatUrl())
                              .preferredGenres(dancerGenreReader.findGenreIdByDancer(dancer))
                              .dancerImages(dancerImageReader.findImageUrlByDancer(dancer))
@@ -114,11 +108,14 @@ public class DancerService {
 
   @Transactional
   public DancerInfoResponse getDancerInfo(Long userId, Long dancerId) {
-    User user = userReader.readUserById(userId);
     Dancer dancer = dancerReader.readDancerById(dancerId);
+    boolean isFavorite = false;
 
-    // 이미 찜이 된 경우 true, 찜이 안되어 있으면 false 저장
-    boolean isFavorite = userDancerReader.readUserDancerByUserAndDancer(user, dancer) != null;
+    if (dancerId != null) {
+      User user = userReader.readUserById(userId);
+      // 이미 찜이 된 경우 true, 찜이 안되어 있으면 false 저장
+      isFavorite = userDancerReader.readUserDancerByUserAndDancer(user, dancer) != null;
+    }
 
     return DancerInfoResponse.builder()
                              .id(dancer.getId())
