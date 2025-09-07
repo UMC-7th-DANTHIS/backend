e95c7a2aa004829c05dc5439c905cd59cb30d288
S0NGMinHyuk <poiuy010724@naver.com>
2025-09-08 01:00:45 +0900

    feat: Week 필드에 enum 타입을 string으로 저장하도록 어노테이션 추가(#106)

--- src/main/java/com/danthis/backend/domain/danceclass/danceclassschedule/DanceClassSchedule.java
+++ src/main/java/com/danthis/backend/domain/danceclass/danceclassschedule/DanceClassSchedule.java
@@ -4,6 +4,8 @@ import com.danthis.backend.domain.BaseEntity;
 import com.danthis.backend.domain.danceclass.DanceClass;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,6 +29,7 @@ public class DanceClassSchedule extends BaseEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @Enumerated(EnumType.STRING)
   @Column
   private Week day;
 
