ad699e81e1d4164abd96d8d90473d6a4a7b95118
S0NGMinHyuk <poiuy010724@naver.com>
2025-09-16 23:23:29 +0900

    feat: 오승민 요청사항 - 게시글, 댓글 응답에 원작자 userId 추가 (#103)

--- src/main/java/com/danthis/backend/api/community/CommunityController.java
+++ src/main/java/com/danthis/backend/api/community/CommunityController.java
@@ -69,7 +69,9 @@ public class CommunityController {
   @Operation(summary = "게시글 단일 조회", description = "게시글을 단일 조회합니다.")
   @GetMapping("/posts/{postId}")
   public ApiResponse<PostReadServiceResponse> getPost(@PathVariable Long postId) {
-    return ApiResponse.OK(postService.getPostById(postId));
+
+    PostReadServiceResponse response = postService.getPostById(postId);
+    return ApiResponse.OK(response);
   }
 
   @Operation(summary = "게시글 목록 조회", description = "게시글 목록을 조회합니다.")
