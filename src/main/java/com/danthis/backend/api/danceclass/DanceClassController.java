bc50bdb149507ae763bbe3dfa3764099a32d3059
S0NGMinHyuk <poiuy010724@naver.com>
2025-11-13 15:36:29 +0900

    fix: 사용자 장르 맞춤 댄스수업 추천 api 구현 (#112)

--- src/main/java/com/danthis/backend/api/danceclass/DanceClassController.java
+++ src/main/java/com/danthis/backend/api/danceclass/DanceClassController.java
@@ -192,4 +192,16 @@ public class DanceClassController {
         averRate);
     return ApiResponse.OK(response);
   }
+
+  @Operation(summary = "추천 댄스 수업 목록 조회 API", description = "사용자 선호 장르기반 추천 댄스수업 목록을 조회합니다.")
+  @GetMapping("/recommendations")
+  @AssignCurrentUserInfo
+  public ApiResponse<DanceClassListServiceResponse> getRecommendedDanceClasses(
+      CurrentUserInfo userInfo,
+      @RequestParam(defaultValue = "4") int size) {
+
+    DanceClassListServiceResponse response =
+        danceClassService.getRecommendedDanceCalssList(userInfo.getUserId(), size);
+    return ApiResponse.OK(response);
+  }
 }
